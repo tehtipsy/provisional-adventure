a3a49bd5e2b5c5da912baa886d0cba9cc5f7b2c3
Kleinzit <mkleinzit@gmail.com>
2023-06-30 17:35:58 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // disabling strict mode since the examples use
+  // useEffect with no dependencies to ensure the function
+  // is only run once and only run on the client.
+  reactStrictMode: false,
+  swcMinify: true,
+  images: {
+    dangerouslyAllowSVG: true,
+    domains: ['static.ably.dev'],
+  },
+}
+
+module.exports = nextConfig
