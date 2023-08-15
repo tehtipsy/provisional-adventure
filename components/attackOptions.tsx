2f23dd6f7e439ca058f7b8d0e281480ab7a9adbf
AS <asafshmueli@gmail.com>
2023-08-15 03:23:53 +0300

    key error fix?

--- components/attackOptions.tsx
+++ components/attackOptions.tsx
@@ -12,7 +12,7 @@ export default function AttackOptions({
 }: AttackOptionsProps) {
   return (
     <>
-      <ul id="options-list">
+      <ul>
         {options.map((option) => (
           <>
             <li key={option}>
