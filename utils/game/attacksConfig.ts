e867b293211aa361d8ebdf08d2200f992c12e95e
AS <asafshmueli@gmail.com>
2023-08-18 10:45:15 +0300

    changed to VERCEL_BRANCH_URL

--- utils/game/attacksConfig.ts
+++ utils/game/attacksConfig.ts
@@ -8,7 +8,7 @@ export const fetchAttackObject = async (
   damageType: string,
   bodyPart: string
 ) => {
-  if (!process.env.SERVER_BASE_URL) {
+  if (!process.env.VERCEL_BRANCH_URL) {
     throw new Error(`Missing environment SERVER_BASE_URL variable.
               If you're running locally, please ensure you have a ./.env file with a value for SERVER_BASE_URL.`);
   }
@@ -17,7 +17,7 @@ export const fetchAttackObject = async (
     bodyPart: bodyPart,
   });
   const response = await fetch(
-    `${process.env.SERVER_BASE_URL}/api/db/wounds?${params}`
+    `http://${process.env.VERCEL_BRANCH_URL}/api/db/wounds?${params}`
   );
   const attackConfig = await response.json();
   console.log("fetchAttackObject: ", attackConfig);
