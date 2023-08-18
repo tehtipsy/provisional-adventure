45b179a7256c0782e7844416f8811cde8700ebb1
AS <asafshmueli@gmail.com>
2023-08-18 10:37:58 +0300

    added process.env.SERVER_BASE_URL

--- utils/game/attacksConfig.ts
+++ utils/game/attacksConfig.ts
@@ -1,10 +1,24 @@
-// call from client and set in poke
-export const fetchAttackObject = async (damageType: string, bodyPart: string) => {
+// "use server";
+// call from client and set in poke ?
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export const fetchAttackObject = async (
+  damageType: string,
+  bodyPart: string
+) => {
+  if (!process.env.SERVER_BASE_URL) {
+    throw new Error(`Missing environment SERVER_BASE_URL variable.
+              If you're running locally, please ensure you have a ./.env file with a value for SERVER_BASE_URL.`);
+  }
   const params = new URLSearchParams({
     damageType: damageType,
     bodyPart: bodyPart,
   });
-  const response = await fetch(`http://0.0.0.0:3000/api/db/wounds?${params}`);
+  const response = await fetch(
+    `${process.env.SERVER_BASE_URL}/api/db/wounds?${params}`
+  );
   const attackConfig = await response.json();
   console.log("fetchAttackObject: ", attackConfig);
   return attackConfig;
